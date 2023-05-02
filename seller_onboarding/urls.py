0a1d9eeccab7fc11181e43cac948bc35b6c45ebe
JordanZaz <zazulakjordan@gmail.com>
2023-05-02 11:38:26 +0200

    url

--- seller_onboarding/urls.py
+++ seller_onboarding/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('onboarding_form/', views.onboarding_form, name='onboarding_form'),
+    path('', views.onboarding_form, name='onboarding_form'),
     path('onboarding_form/save_data/',
          views.save_form_data, name='save_form_data'),
     path('thank_you/', views.thank_you, name='thank_you'),
